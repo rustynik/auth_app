88ce3c2559a4d6cf5d8538ac628b83b04f7ea62d
andrei nikolaev <a.nikolaev@virtusystems.ru>
2018-08-15 13:03:19 +0300

    tiny refactoring

--- src/main.rs
+++ src/main.rs
@@ -32,24 +32,22 @@ use hyper::service::service_fn;
 mod settings;
 use std::env;
 
-fn get_settings() -> settings::ApplicationSettings {
+fn get_settings_path() -> String {
     let args: Vec<String> = env::args().collect();
 
-    let path = match args.len() {
+    match args.len() {
         2 => args[1].to_owned(),
         _ => {
             let mut dir = env::current_exe().expect("Cannot get current directory");
             dir.set_file_name("config.json");
             dir.to_str().expect("Invalid path").to_owned()
         }
-    };
-
-    settings::read(&path)
+    }
 }
 
 fn main() {
     
-    let app_settings = get_settings();
+    let app_settings = settings::read(&get_settings_path());
 
     let addr = ([127, 0, 0, 1], app_settings.port).into();
     
